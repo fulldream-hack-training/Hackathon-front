5f0a8217bb3b32a488f55e06b1427e703ef3c058
Fiantso Harena <fiantsorav@gmail.com>
2024-01-26 20:23:55 +0300

    chore: implement auth layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import './global.css';
+import AuthLayout from './layouts/AuthLayout';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Routes>
+      <Route path='/auth' element={<AuthLayout />}>
+        {/* Public routes */}
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+      </Route>
+    </Routes>
+  );
 }
