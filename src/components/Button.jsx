9c9aa7ab94e72ce40f3cc0e5f6ba4c878e07de80
Fiantso Harena <fiantsorav@gmail.com>
2024-01-27 02:39:10 +0300

    style: change button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ styles }) => (
   <button
     type='button'
-    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+    className={`py-4 px-6 font-poppins text-[18px] text-white bg-[#8371fd] rounded-[10px] outline-none font-semibold ${styles}`}
   >
     Get Started
   </button>
