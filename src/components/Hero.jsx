59cf591862dd5a09a2e61693982a7eee9e286eaa
Fiantso Harena <fiantsorav@gmail.com>
2024-01-27 02:25:19 +0300

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,39 @@
+import styles from '../style';
+import { discount, robot } from '../assets';
+import GetStarted from './GetStarted';
+
+const Hero = () => {
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            Welcome to
+            <br className="sm:block hidden" />
+            <span className="text-gradient">Dreamy</span>
+          </h1>
+        </div>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt deleniti in maxime possimus veniam
+            error a quasi eius quidem soluta qui, optio reprehenderit illo quo, similique dolores quia numquam!
+          </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
