f0de7fe2b724c4ce1eb9e177a39eb4fa1ae97190
Fiantso Harena <fiantsorav@gmail.com>
2024-01-27 03:25:13 +0300

    chore: wrap homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import { Business, CTA, Hero, Navbar, Testimonials } from './components';
-import styles from './style';
+import HomePage from "./pages/HomePage";
 
 const App = () => (
-  <div className='bg-primary w-full overflow-hidden'>
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
-      </div>
-    </div>
-
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
-
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Business />
-        <Testimonials />
-        <CTA />
-      </div>
-    </div>
-  </div>
+  <>
+    <HomePage />
+  </>
 );
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,29 @@
+import { Business, CTA, Hero, Navbar, Testimonials } from '../components';
+import styles from '../style';
+
+
+export default function HomePage() {
+    return (
+        <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Business />
+        <Testimonials />
+        <CTA />
+      </div>
+    </div>
+  </div>
+    );
+}
