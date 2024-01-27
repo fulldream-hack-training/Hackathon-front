8c5183826654c652e3afea5972d564f158b97158
Fiantso Harena <fiantsorav@gmail.com>
2024-01-27 06:43:06 +0300

    chore: implement OnBoarding pages

---
+++ src/pages/onBoarding/OnBoarding.jsx
@@ -0,0 +1,11 @@
+import OnBoardingCSS from './OnBoarding.module.css';
+
+export default function OnBoarding() {
+  return (
+    <>
+      <div className='bg-primary w-[100vw] h-[100vh]'>
+        
+      </div>
+    </>
+  );
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,7 +1,8 @@
 import AuthLayout from '../layouts/AuthLayout';
 import HomePage from '../pages/HomePage';
-import NotFound from '../pages/notFound/NotFound';
 import Login from '../pages/login/Login';
+import NotFound from '../pages/notFound/NotFound';
+import OnBoarding from '../pages/onBoarding/OnBoarding';
 import Register from '../pages/register/Register';
 import { Route, Routes } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ export default function Router() {
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='*' element={<NotFound />} />
+      <Route path='/on-boarding' element={<OnBoarding />} />
       <Route path='/auth' element={<AuthLayout />}>
         <Route path='login' element={<Login />} />
         <Route path='register' element={<Register />} />
