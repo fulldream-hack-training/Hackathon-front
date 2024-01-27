d387087af36872a74a8272500a021edfd2b6791c
Fiantso Harena <fiantsorav@gmail.com>
2024-01-27 03:53:14 +0300

    chore: configure button to point to /auth/login

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Button = ({ styles }) => (
   <button
     type='button'
     className={`py-4 px-6 font-poppins text-[18px] text-black bg-[#00f6ff] rounded-[10px] outline-none font-semibold ${styles}`}
   >
-    Get Started
+    <Link to={'/auth/login'}>Get Started</Link>
   </button>
 );
 
