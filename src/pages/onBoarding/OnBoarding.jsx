8b8fdcb91ddfdcc6a7e94fad0b9e279837788156
Fiantso Harena <fiantsorav@gmail.com>
2024-01-27 08:25:29 +0300

    style: on boarding styles

--- src/pages/onBoarding/OnBoarding.jsx
+++ src/pages/onBoarding/OnBoarding.jsx
@@ -1,11 +1,35 @@
 import OnBoardingCSS from './OnBoarding.module.css';
+import { Avatar } from '@mui/material';
 
 export default function OnBoarding() {
   return (
-    <>
-      <div className='bg-primary w-[100vw] h-[100vh]'>
-        
+    <main>
+      <div className={OnBoardingCSS.box}>
+        <div className={OnBoardingCSS.inner_box}>
+          <div className={OnBoardingCSS.heading}>
+            <h1>Here we are !</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam alias fuga numquam magni, iste,
+              veniam aliquid quisquam excepturi repudiandae officiis! Expedita dicta libero aliquam atque sequi quidem
+              sint eaque.
+            </p>
+            <div className={OnBoardingCSS.box_container}>
+              <div className={OnBoardingCSS.container}>
+                <Avatar />
+                <h4>Fiantso Harena</h4>
+              </div>
+              <div className={OnBoardingCSS.container}>
+                <Avatar />
+                <h4>Fiantso Harena</h4>
+              </div>
+              <div className={OnBoardingCSS.container}>
+                <Avatar />
+                <h4>Fiantso Harena</h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </main>
   );
 }
